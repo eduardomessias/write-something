7ce30565a2b0fe6d9c39d2036499a064554f75e7
eduardomessias <eduardo.messias@outlook.com>
2021-07-22 23:30:16 +0100

    google docs clone

--- server/server.js
+++ server/server.js
@@ -1,3 +1,18 @@
+const mongoose = require('mongoose')
+const MONGO_URI = 'mongodb+srv://god:8fcgAQTPQbWfh5CE@cluster0.tvme0.mongodb.net/write-something?retryWrites=true&w=majority'
+const MONGOOSE_OPTIONS = {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false,
+    useCreateIndex:true
+}
+mongoose.connect(MONGO_URI,MONGOOSE_OPTIONS)
+
+
+const Document = require('./Document')
+const DEFAULT_VALUE = ""
+
+
 const io = require ('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
@@ -5,6 +20,26 @@ const io = require ('socket.io')(3001, {
     }
 })
 
-io.on ("connection", socket => {
+
+io.on ("connection",socket => {
+    socket.on('get-document',async documentId => {
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        socket.emit('load-document',document.data)
+        socket.on('send-change',delta => {
+            socket.broadcast.to(documentId).emit('receive-change',delta)
+        })
+        socket.on('save-document',async data => {
+            await Document.findByIdAndUpdate(documentId,{data})
+        })
+    })
     console.log("connected")
 })
+
+
+async function findOrCreateDocument(id) {
+    if (id == null) return
+    const document = await Document.findById(id)
+    if (document) return document
+    return await Document.create({_id:id,data:DEFAULT_VALUE})
+}
